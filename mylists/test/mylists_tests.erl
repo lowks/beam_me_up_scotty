3ea8f3d9bcd6f530722e8a9188c38702789c0354
Muhammad Lukman Low <kianseong@gmail.com>
2018-05-18 23:17:01 +0800

    Adding name to tests

--- mylists/test/mylists_tests.erl
+++ mylists/test/mylists_tests.erl
@@ -3,7 +3,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 drop_nth_element_test_() ->
-        [ ?_assertEqual([0,1], mylists:drop_nth_element([3,0,1], 1)),
-          ?_assertEqual([1], mylists:drop_nth_element([0,1], 1)),
-          ?_assertEqual([0], mylists:drop_nth_element([0], 0))
+         [{ "Testing normal drop element", 
+          ?_assertEqual([0,1], mylists:drop_nth_element([3,0,1], 1))},
+          { "Testing normal drop element",
+          ?_assertEqual([1], mylists:drop_nth_element([0,1], 1))},
+          { "Testing drop element from empty list",
+          ?_assertEqual([], mylists:drop_nth_element([], 1))},
+          { "Testing drop n bigger than length of list",
+          ?_assertEqual([1,2], mylists:drop_nth_element([1,2], 10))}   
          ].
