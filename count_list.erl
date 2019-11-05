26ff54964701626492793062b696e11aaf046a57
Low Kian Seong <kianseong@gmail.com>
2019-11-05 20:44:04 +0800

    Adding tests to count_list

--- count_list.erl
+++ count_list.erl
@@ -1,8 +1,12 @@
 -module(count_list).
+-include_lib("eunit/include/eunit.hrl").
 -export([main/0]).
 
 list_length([]) -> 0;
 list_length([_|T]) -> 1 + list_length(T).
+
+list_length_test() ->
+     ?assert(list_length([1,2,3,4,5]) == 5).
 
 main() ->
 	[X || X <- [1, 2, 3, 4, 5, 6], is_integer(X), X rem 2 == 0],
