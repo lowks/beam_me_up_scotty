337dc42439a05d63cca7d02950906aa5c5082648
Muhammad Lukman Low <lowkster.etiqa@gmail.com>
2021-09-23 21:55:54 +0800

    adding codewars product, century and strong

--- codewars/src/codewars.erl
+++ codewars/src/codewars.erl
@@ -52,6 +52,116 @@ summation(0) -> 0;
 summation(N) -> N + summation(N-1).
 
 
+% In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+
+% pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+% The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+% --the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+% --the second pair is (5,8) and are not consecutive
+% --the third pair is (-4,-3), consecutive. Count = 2
+% --the fourth pair is (7,6), also consecutive. Count = 3. 
+% --the last digit has no pair, so we ignore.
+% More examples in the test cases.
+
+% -module(kata).
+% -export([pairs/1, pairs/2]).
+
+pairs([]) ->
+  0;
+
+pairs(List) ->
+  pairs(List, 0).
+
+pairs(L, Acc) when length(L) =< 1 ->
+   Acc;
+
+pairs([X,Y| Tail], Acc) when abs(Y-X) =:= 1 ->
+   pairs(Tail, Acc+1);
+
+pairs([X,Y | Tail], Acc) when Y =/= X+1 ->
+   pairs(Tail, Acc).
+
+% Definition
+% Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+
+% For example: 145, since
+% 1! + 4! + 5! = 1 + 24 + 120 = 145
+% So, 145 is a Strong number.
+
+strong(N) ->
+  strong(integer_to_list(N), N, 0).
+
+strong([], N, Acc) ->
+  if 
+    N =/= Acc ->
+      "Not Strong !!";
+    N =:= Acc ->
+      "STRONG!!!!"
+  end;
+  
+strong([H|T], N, Acc) ->
+  strong(T, N, Acc+factorial(H -$0)).
+
+factorial(0) -> 1;
+factorial(N) when N > 0 ->
+  N * factorial(N-1).
+
+
+% The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+% Task :
+% Given a year, return the century it is in.
+
+% Input , Output Examples :
+% 1705 --> 18
+% 1900 --> 19
+% 1601 --> 17
+% 2000 --> 20
+
+century(Year) ->
+  if
+    Year =< 999 ->
+      List = string:right(integer_to_list(Year), 4, $0);
+    Year >= 1000 -> 
+      List = integer_to_list(Year)
+  end,
+  Century = list_to_integer(lists:sublist(List, length(List) - 2)),
+  Year2 = list_to_integer(integer_to_list(Year) -- integer_to_list(Century)),
+  if
+    Year2 >= 1 ->
+      Century + 1;
+    Year2 =:= 0 ->
+      Century
+   end.
+
+century_better(Year) ->
+  ((Year-1) div 100)+1.
+
+% Task
+% Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+% Notes
+% Array/list size is at least 3 .
+
+% Array/list's numbers Will be mixture of positives , negatives and zeros
+
+% Repetition of numbers in the array/list could occur.
+
+
+max_product(A, S) ->
+  List = lists:reverse(lists:sort(A)),
+  Reversed = lists:sublist(List,S),
+  product(Reversed).
+
+product(List) ->
+  product(List, 1).
+
+product([], Result) ->
+  Result;
+
+product([H|T], Result) ->
+  product(T, Result*H).
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
